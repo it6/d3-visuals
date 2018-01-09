d45af99bd458389a5d81561fa0e38ecc50eee09a
Surya Kotamraju <kotamraju@me.com>
2018-01-09 07:38:38 -0500

    use 960 as width and 500 as height

--- 999999-visual-base-files/script.js
+++ 999999-visual-base-files/script.js
@@ -1,11 +1,10 @@
 window.onload = () => {
-  const margin = { top: 20, bottom: 20, left: 20, right: 20 },
-    width = window.innerWidth - margin.left - margin.right,
-    height = window.innerHeight - margin.top - margin.bottom,
+  const width = 960,
+    height = 500,
     svg = d3
       .select("body")
       .append("svg")
-      .attr("width", 960)
-      .attr("height", 500),
+      .attr("width", width)
+      .attr("height", height),
     primaryGroup = svg.append("g").attr("transform", `translate(480, 250)`);
 };
