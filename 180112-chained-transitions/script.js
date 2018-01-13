197b1b0982828c307f94800db049806a3918a969
Surya Kotamraju <kotamraju@me.com>
2018-01-13 16:58:12 -0500

    add transition

--- 180112-chained-transitions/script.js
+++ 180112-chained-transitions/script.js
@@ -6,20 +6,35 @@ window.onload = () => {
       .append("svg")
       .attr("width", width)
       .attr("height", height),
-    primaryGroup = svg.append("g").attr("transform", `translate(40, 250)`),
+    primaryGroup = svg.append("g").attr("transform", `translate(20, 20)`),
+    colorScale = d3
+      .scaleLinear()
+      .domain([10, 0])
+      .range(["hsl(62, 100%, 90%)", "hsl(228, 30%, 20%)"])
+      .interpolate(d3.interpolateHcl),
+    y = d3
+      .scalePoint()
+      .domain(d3.range(50))
+      .range([10, height]),
     circles = primaryGroup
       .selectAll("circle")
-      .data(d3.range(30))
+      .data(y.domain())
       .enter()
       .append("circle")
-      .attr("cx", d => d * d)
-      .attr("cy", 10)
-      .attr("fill", "red")
-      .attr("stroke-width", "10")
-      .attr("r", 10);
-
-  const y = d3
-    .scaleLinear()
-    .domain(d3.range(30))
-    .range([0, height]);
+      .attr("cx", 0)
+      .attr("cy", y)
+      .attr("fill", d => colorScale(Math.abs(d % 20 - 10)))
+      .attr("r", 10)
+      .transition()
+      .duration(2500)
+      .delay(d => d * 4)
+      .on("start", function repeat() {
+        d3
+          .active(this)
+          .attr("cx", width - 100)
+          .transition()
+          .attr("cx", 0)
+          .transition()
+          .on("start", repeat);
+      });
 };
