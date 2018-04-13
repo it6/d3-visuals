c4dbf0a71a7c560fc460f4a3091108e813a13a57
Surya Kotamraju <kotamraju@me.com>
2018-04-12 21:57:30 -0400

    barchart - add more bars

--- 180409-working/script.js
+++ 180409-working/script.js
@@ -1,11 +1,36 @@
 window.onload = () => {
-  const width = 400,
-    height = 600,
-    data = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  const width = 960,
+    height = 500,
+    data = [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20
+    ],
     yScale = d3
       .scaleLinear()
       .domain([0, d3.max(data)])
-      .range([0, height / 1.5]),
+      .range([height - 100, 0]),
+    xScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data)])
+      .range([0, width]),
     svg = d3
       .select('body')
       .append('svg')
@@ -17,8 +42,8 @@ window.onload = () => {
     .data(data)
     .enter()
     .append('rect')
-    .attr('x', d => 0)
+    .attr('x', d => yScale(d))
     .attr('y', d => yScale(d))
-    .attr('height', d => 10)
-    .attr('width', d => yScale(d));
+    .attr('height', d => height - yScale(d))
+    .attr('width', d => 5);
 };
